1207d07580144b6f7dfb7d433c597f4b8e21e7e4
Chhorvy Om <vivi@chhorvys-imac.attlocal.net>
2022-03-25 10:32:58 -0700

    Task 2: Used the for loop to find summation of 4

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ function myFunction() {
 //myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
-
-
+/*The nested function can access the variable internal because the variable is within the lexical scope
+of the same function.
+*/
 
 
 
@@ -30,11 +31,14 @@ function myFunction() {
 💡 NOTE: you may use a for loop for this function if you wish 
 */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(num1) {
+  let count = 0;
+  for (let i = 0; i <= num1; i++) {
+    count += i;
   }
- 
+  return count;
+};
+ //console.log(summation(4));
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
